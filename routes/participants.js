b5e703c0bfbea881b11fa5461ca855c1e471dbf4
naaoufal <naoufelbenmensour@gmail.com>
2021-02-18 23:50:55 +0100

    add structure files

--- routes/participants.js
+++ routes/participants.js
@@ -2,21 +2,13 @@ const express = require("express")
 const routerParticipants = express.Router()
 
 // // declare our routes
-const { register, participants } = require("../controllers/participants")
+const { register, all } = require("../controllers/participants")
 
 
 
-routerParticipants.get("/all", participants)
+routerParticipants.get("/all", all)
 
 routerParticipants.post("/register", register)
-
-// router.get("/all" , (req, res, next) => {
-//     res.send("this is a get test")
-// })
-
-// router.post("/register", (req, res, next) => {
-//     res.send("this is a register test")
-// })
 
 
 module.exports = routerParticipants
--- server.js
+++ server.js
@@ -3,23 +3,10 @@ const mongoose = require("mongoose");
 const app = express();
 
 
-// connect our database:
-mongoose.connect("mongodb://localhost/million", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-const db = mongoose.connection
-
-db.on("error", (error) => console.error(error))
-db.once("open", () => console.log("Connected to DataBase !!!"))
-
-app.use(express.json())
 
 
 // use routes
-const participantsroute = require("./routes/participants.js");
-app.use("/api/participants", participantsroute)
-
+app.use("/api/participants", require("./routes/participants.js"))
 
 // start the server
 app.listen(4000, () => console.log("the server is started"));
