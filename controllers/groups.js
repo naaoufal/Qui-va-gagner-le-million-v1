5dd33f1b501b2da23400648a1aaf294b4014f7fd
naaoufal <naoufelbenmensour@gmail.com>
2021-02-22 00:18:12 +0100

    push some modification

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,31 +1,59 @@
+const { json } = require('body-parser')
 const Groups = require('../models/groups')
 
-async function add (req, res) {
+// create new groupe with validation
+function add (req, res) {
     const groups = new Groups({
         idparticipant : req.body.idparticipant,
         groupcode : req.body.groupcode
     })
-    try {
-        const newGroups = await groups.save()
-        res.json(newGroups)
-    } catch (error) {
-        res.json({message : error.message})
-    }
+    const query = {idparticipant : groups.idparticipant}
+    //console.log(query)
+
+    Groups.find(query, async (err, data) => {
+        var parti_id = data.map((info) => {
+            return info.idparticipant
+        })
+        console.log(parti_id.length)
+        if(parti_id.length >= 1){
+            res.json({message : "You Already Join a Group"})
+        } else {
+            const newGroups = await groups.save()
+            res.json(newGroups)
+        }
+    }).catch(err => {
+        console.log(err)
+    })
 }
 
-async function joinGroup (req, res) {
+// join Group with validation
+function joinGroup (req, res) {
     const groups = new Groups({
         idparticipant : req.body.idparticipant,
         groupcode : req.body.groupcode
     })
-    try {
-        const joinGroup = await groups.save()
-        res.json(joinGroup)
-    } catch (error) {
-        res.json({message : error.message})
-    }
+    const query = {idparticipant : groups.idparticipant, groupcode : groups.groupcode}
+    //console.log(query)
+    Groups.find(query, async (err, data) => {
+        var code = data.map((info) => {
+            return info.groupcode
+        })
+        var part_id = data.map((info) => {
+            return info.idparticipant
+        })
+        console.log(part_id.length)
+        console.log(code.length)
+        if(code.length >= 4 || part_id.length >= 1) {
+            //console.log("hbess")
+            res.json({message : "Group Full Or You ALready Join this Group !!!"})
+        } else {
+            const joinGroup = await groups.save()
+            res.json(joinGroup)
+        }
+    })
 }
 
+// fetch all Groups 
 async function all (req, res) {
     try {
         const groups = await Groups.find()
